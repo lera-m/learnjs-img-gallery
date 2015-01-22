965d4bfe71ab3ba1da45697b9c572598f782f06b
lera <lera@example.test>
2015-01-23 00:36:21 +0200

    die Bildergröße ändern

--- script.js
+++ script.js
@@ -8,12 +8,15 @@ var i;
 for (i = 0;  i < images.length; i++) {
 	image = images[i];
 	image.addEventListener('click', function(event) {
+		
        var image = event.target;
        var src = image.src;
        var preImg = document.getElementById('preImg');
-       preImg.src = image.src;
-       console.log(src);
 
+	   src = src.replace('200','600');
+	   src = src.replace('150','450');
+
+       preImg.src = src;
 	});
 
-}
+} 
