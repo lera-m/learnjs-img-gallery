0874075260512c893974c7b9de8ccee1878c2657
evanvosberg <evanvosberg@inext.me>
2015-01-22 23:39:44 +0100

    Clean up.

--- script.js
+++ script.js
@@ -1,22 +1,19 @@
 var images = document.getElementsByTagName('img');
 var image;
-
-
 var i;
 
 
-for (i = 0;  i < images.length; i++) {
+for (i = 0; i < images.length; i++) {
 	image = images[i];
-	image.addEventListener('click', function(event) {
-		
-       var image = event.target;
-       var src = image.src;
-       var preImg = document.getElementById('preImg');
 
-	   src = src.replace('200','600');
-	   src = src.replace('150','450');
+	image.addEventListener('click', function (event) {
+		var image = event.target;
+		var src = image.src;
+		var preImg = document.getElementById('preImg');
 
-       preImg.src = src;
+		src = src.replace('200', '600');
+		src = src.replace('150', '450');
+
+		preImg.src = src;
 	});
-
-} 
+}
