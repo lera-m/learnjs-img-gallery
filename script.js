5d08d064fdb4ec207598411c1b862c5a681fb9fd
lera <lera@example.test>
2015-01-23 00:14:35 +0200

    das Bild ändert sich mit onclick

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
 var images = document.getElementsByTagName('img');
 var image;
 
+
 var i;
 
 
-for (i = 0;  i < images.lenght; i++) {
+for (i = 0;  i < images.length; i++) {
 	image = images[i];
-	image.addEventListener('onclick', function() {
-       alert('test');
+	image.addEventListener('click', function(event) {
+       var image = event.target;
+       var src = image.src;
+       var preImg = document.getElementById('preImg');
+       preImg.src = image.src;
+       console.log(src);
+
 	});
 
 }
