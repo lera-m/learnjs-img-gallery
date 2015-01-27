8d87c516821938761d67e79e9008d0ea51d7a73c
lera <lera@example.test>
2015-01-28 01:28:55 +0200

    function replacement for duplication of code

--- script.js
+++ script.js
@@ -6,19 +6,23 @@ var previous = document.getElementsByClassName('prebutton');
 var nextbutton;
 var prebutton;
 var ImageCnt = 0;
+var imgFunction = function(image) {
+		if (image){
+		    var src = image.src;
+			var preImg = document.getElementById('preImg');
+
+			src = src.replace('200', '600');
+			src = src.replace('150', '450');
+
+			preImg.src = src;
+		}
+}
 
 for (i = 0; i < images.length; i++) {
 	image = images[i];
 
 	image.addEventListener('click', function (event) {
-		var image = event.target;
-		var src = image.src;
-		var preImg = document.getElementById('preImg');
-
-		src = src.replace('200', '600');
-		src = src.replace('150', '450');
-
-		preImg.src = src;
+		imgFunction(event.target);
 	});
 }
 
@@ -26,33 +30,14 @@ for (i = 0; i < next.length; i++) {
      nextbutton = next[i];
      nextbutton.addEventListener('click', function (event) {
      	ImageCnt++;
-     	var image = images[ImageCnt];
-     	if (image){
-	     	var src = image.src;
-			var preImg = document.getElementById('preImg');
-
-		    src = src.replace('200', '600');
-			src = src.replace('150', '450');
-
-			preImg.src = src;	
-     	}
-     	
+	    imgFunction(images[ImageCnt]);  	     
      });
 }
+
 for (i = 0; i < previous.length; i++) {
      prebutton = previous[i];
      prebutton.addEventListener('click', function (event) {
      	ImageCnt--;
-     	var image = images[ImageCnt];
-     	if (image){
-	     	var src = image.src;
-			var preImg = document.getElementById('preImg');
-
-		    src = src.replace('200', '600');
-			src = src.replace('150', '450');
-
-			preImg.src = src;	
-     	}
-     	
+     	imgFunction(images[ImageCnt]); 
      });
 }
