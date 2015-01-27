f1497345fa29f7031b1c163ceb05d31065182d2d
lera <lera@example.test>
2015-01-28 01:07:15 +0200

    next and previous buttons

--- script.js
+++ script.js
@@ -1,7 +1,11 @@
 var images = document.getElementsByTagName('img');
 var image;
 var i;
-
+var next = document.getElementsByClassName('nextbutton');
+var previous = document.getElementsByClassName('prebutton');
+var nextbutton;
+var prebutton;
+var ImageCnt = 0;
 
 for (i = 0; i < images.length; i++) {
 	image = images[i];
@@ -16,13 +20,39 @@ for (i = 0; i < images.length; i++) {
 
 		preImg.src = src;
 	});
-	var ImageCnt = 0;
-
-    function nextImg(){
-	   ImageCnt++;
-       document.getElementById("preImg").innerHTML = image[ImageCnt];
-    }; 
 }
 
+for (i = 0; i < next.length; i++) {
+     nextbutton = next[i];
+     nextbutton.addEventListener('click', function (event) {
+     	ImageCnt++;
+     	var image = images[ImageCnt];
+     	if (image){
+	     	var src = image.src;
+			var preImg = document.getElementById('preImg');
 
-   
+		    src = src.replace('200', '600');
+			src = src.replace('150', '450');
+
+			preImg.src = src;	
+     	}
+     	
+     });
+}
+for (i = 0; i < previous.length; i++) {
+     prebutton = previous[i];
+     prebutton.addEventListener('click', function (event) {
+     	ImageCnt--;
+     	var image = images[ImageCnt];
+     	if (image){
+	     	var src = image.src;
+			var preImg = document.getElementById('preImg');
+
+		    src = src.replace('200', '600');
+			src = src.replace('150', '450');
+
+			preImg.src = src;	
+     	}
+     	
+     });
+}
